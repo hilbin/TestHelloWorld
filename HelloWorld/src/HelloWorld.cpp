2cbef67f1be7d23401badf854905f85fd8d32f76
BZ <BZ@192.168.0.15>
2019-07-23 20:21:47 -0500

    Initialize the Hello World project

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : BZ
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {unresolved
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
